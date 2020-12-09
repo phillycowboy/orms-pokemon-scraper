2c4f31323aa06f8ffea6016af26dacab107bde10
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-12-09 18:31:00 -0500

    commit tree a2becae1d3174d82fc1d3ba8363f429f02eb7705

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,29 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+    def initialize(id:, name:, type:, db:)
+        @id = id 
+        @name = name
+        @type = type 
+        @db = db 
+    end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?, ?)
+        SQL
+        db.execute(sql, name, type)
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+        SELECT *
+        FROM pokemon
+        WHERE id = ?
+        LIMIT 1
+        SQL
+       pokemon_data = db.execute(sql, id).flatten
+       Pokemon.new(id:pokemon_data[0], name:pokemon_data[1], type:pokemon_data[2], db:db)
+    end
+
 end
